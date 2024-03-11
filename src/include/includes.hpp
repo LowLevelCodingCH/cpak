9dffc566f318db4f691784acbeddfa7d3339ad0a
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-03-12 00:18:45 +0300

    Add files via upload

---
+++ src/include/includes.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "getf.hpp"
+#include <socketapi.h>
+#include <netfw.h>
+#include <Windows.h>
+#include <memory.h>
+#include <math.h>
---
+++ src/main.cc
@@ -0,0 +1,5 @@
+#include "include/includes.hpp"
+
+int main(void*){
+    return 0;
+}
