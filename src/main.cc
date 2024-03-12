9a9a5702bdd3416e79f38caba9a5ca9f2c39e6c0
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-03-12 21:32:40 +0300

    Delete src/main.cc

--- src/main.cc
+++
@@ -1,9 +0,0 @@
-#include "include/includes.hpp"
-
-int main(int argv, char* argc[]){
-    std::string pak = argc[1];
-    std::string package = APIKEY + pak;
-    std::string command = "git clone " + package + " C:\\cpak\\libs\\pkgs\\";
-    std::system(command.c_str());
-    return 0;
-}
