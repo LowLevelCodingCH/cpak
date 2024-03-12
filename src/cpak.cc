cb3833b8e023de7f11ac1683fc3b110813f66921
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-03-12 21:57:36 +0300

    Add files via upload

--- src/cpak.cc
+++ src/cpak.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]){
     if(argc < 3){
-        std::cout << "USAGE: cpak <package> <c/cc>";
+        std::cout << "USAGE: cpak <package>";
         return 1;
     }
     else{
--- src/include/includes.hpp
+++ src/include/includes.hpp
@@ -7,11 +7,5 @@
 
 // main incs
 #include <iostream>
-#include <socketapi.h>
-#include <netfw.h>
-#include <Windows.h>
-#include <memory.h>
-#include <math.h>
-#include <vector>
 #include <cstdlib>
 #include <string.h>
