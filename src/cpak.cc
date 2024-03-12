b91812ac670631fc4f81c0a60ad9f560a5de3c9d
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-03-13 00:19:42 +0300

    Add files via upload

--- src/cpak.cc
+++ src/cpak.cc
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
     else{
         std::string pak = argv[1];
         std::string package = "https://github.com/cpakmanager/" + pak;
-        std::string command = "git clone " + package + " C:\\cpak\\libs\\pkgs\\" + package + "\\";
+        std::string command = "git clone " + package + " C:\\Program Files\\LLVM\\lib\\clang\\12.0.0\\include" + pak + "\\"; // for later stuff with it, clang install dir: C:\Program Files\LLVM\lib\clang\12.0.0\include, and C:\Program Files (x86)\mingw64\lib\gcc\x86_64-w64-mingw32\13.1.0\include for gcc
         std::system(command.c_str());
         return 0;
     }
